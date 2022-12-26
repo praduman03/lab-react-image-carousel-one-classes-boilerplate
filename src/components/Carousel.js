bda25e4d4a5920e02064ef7923f0fc572b4132d1
Praduman03 <praduman03k@gmail.com>
2022-12-26 10:48:50 +0530

    task done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,10 +4,44 @@ import { images } from "../data/CarouselData";
 // you can explore more - and check as how to use materiul ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Construction } from "@mui/icons-material";
 
 // implement the class below
 class Carousel extends Component {
-  
+    constructor(){
+        super()
+        this.state={
+            count:0
+        }
+    }
+    front(){
+        if(this.state.count===2){
+        this.setState({count: 0})
+        }
+        else{
+            this.setState({count: this.state.count+1})
+        }
+    }
+    back(){
+        if(this.state.count===0)
+        this.setState({count: 2})
+        else
+        this.setState({count: this.state.count-1})
+    }
+    render(){
+        return(
+    <div className="container">
+    <ArrowBackIosIcon onClick={()=>this.back()} className="arrowback"/>
+    <div className="imgCard">
+        <h2>{images[this.state.count].title}</h2>
+        <h2>{images[this.state.count].subtitle}</h2>
+        <img src={images[this.state.count].img} alt="" />
+    </div>
+    <ArrowForwardIosIcon onClick={()=>this.front()} className="arrowfront"/>
+    </div>
+        )
+    }
+
 }
 
 export default Carousel;
